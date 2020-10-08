85d134bd20262281d4fb0e6233a70cba1a608a65
yasmin <zhamborova.y@gmail.com>
2020-10-08 12:25:36 +0300

    course editor css

---
+++ src/components/LessonTabsComponent/LessonTabs.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const LessonTabs = () => {
+
+   return (<ul className="nav nav-tabs">
+       <li className="nav-item">
+           <a href="#" className="nav-link">Lesson 1</a>
+       </li>
+       <li className="nav-item">
+           <a href="#" className="nav-link active">Lesson 2</a>
+       </li>
+       <li className="nav-item">
+           <a href="#" className="nav-link">Lesson 3</a>
+       </li>
+       <li className="nav-item">
+           <a href="#" className="nav-link">Lesson 4</a>
+       </li>
+       <li className="nav-item">
+           <a href="#" className="nav-link">Lesson 5</a>
+       </li>
+       <li className="nav-item">
+           <a href="#" className="nav-link">Lesson 1</a>
+       </li>
+   </ul>)
+
+}
+
+export default LessonTabs;
---
+++ src/pages/CourseEditor/CourseEditor.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const CourseManager = () =>{
+
+    return( <div className="container-fluid">
+            <div className="wbdv-lesson-tabs">
+            </div>
+
+            <div className="row">
+                <div className="col-3"></div>
+                <div className="col-5"></div>
+        </div>
+        </div>
+
+
+
+    )
+
+
+}
